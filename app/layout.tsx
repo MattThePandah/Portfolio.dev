c4bbb22db3a2879b13051c06a936252a8b55327b
Matt <pandahchan@outlook.com>
2024-06-22 23:58:37 +0100

    Some files didn't transfer over

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import 'css/tailwind.css'
-import 'pliny/search/algolia.css'
-import 'remark-github-blockquote-alert/alert.css'
-
-import { Space_Grotesk } from 'next/font/google'
-import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
-import Footer from '@/components/Footer'
+import { Analytics, AnalyticsConfig } from 'pliny/analytics'
+
 import siteMetadata from '@/data/siteMetadata'
+import 'css/tailwind.css'
+import 'pliny/search/algolia.css'
+import './globals.css'
+// import 'remark-github-blockquote-alert/alert.css'
+
+import { Space_Grotesk } from 'next/font/google'
+import { SearchProvider, SearchConfig } from 'pliny/search'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
 
@@ -74,7 +76,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
+      <body className="bg-background text-black antialiased dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,87 @@
-import siteMetadata from '@/data/siteMetadata'
+'use client'
+
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import siteMetadata from '@/data/siteMetadata'
+import { cn } from '@/scripts/utils/tailwind-helpers'
+import NextImage from 'next/image'
+import { useEffect, useState } from 'react'
+
 import Link from './Link'
 import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import ThemeSwitch from './ThemeSwitch'
 
 const Header = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false)
+
   return (
-    <header className="flex items-center justify-between py-10">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
+    <header className="fixed inset-x-0 top-4 z-40 flex h-[60px] justify-center">
+      <div className={cn(
+      'mx-6 w-full max-w-[375px] items-center justify-between rounded-3xl border border-border bg-secondary px-4 shadow-sm saturate-100 backdrop-blur-[10px] sm:max-w-screen-sm xl:max-w-screen-xl',
+      isScrolled && 'border-transparent bg-background/80'
+      )}>
+        <div className="mx-auto flex h-[60px] w-full items-center justify-between">
+          <div>
+            <Link href="/" aira-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between rounded-full">
+              <NextImage
+                src="/static/images/logo.webp"
+                alt="Logo"
+                width="40"
+                height="40"
+                title="Logo"
+              />
             </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>
-      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className="hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400
-              sm:block"
-            >
-              {link.title}
             </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
+          </div>
+          <div className="flex items-center sm:space-x-3">
+            <ul className="hidden space-x-2 sm:flex">
+              {headerNavLinks.map((link, i) => (
+                <li key={i}>
+                  
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     </header>
+    // <header className="fixed inset-x-0 top-4 z-40 flex h-[60px] justify-center">
+    //   <div>
+    //     <Link href="/" aria-label={siteMetadata.headerTitle}>
+    //       <div className="flex items-center justify-between">
+    //         <div className="mr-3">
+    //           <Logo />
+    //         </div>
+    //         {typeof siteMetadata.headerTitle === 'string' ? (
+    //           <div className="hidden h-6 text-2xl font-semibold sm:block">
+    //             {siteMetadata.headerTitle}
+    //           </div>
+    //         ) : (
+    //           siteMetadata.headerTitle
+    //         )}
+    //       </div>
+    //     </Link>
+    //   </div>
+    //   <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
+    //     {headerNavLinks
+    //       .filter((link) => link.href !== '/')
+    //       .map((link) => (
+    //         <Link
+    //           key={link.title}
+    //           href={link.href}
+    //           className="hidden font-medium text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400
+    //           sm:block"
+    //         >
+    //           {link.title}
+    //         </Link>
+    //       ))}
+    //     <SearchButton />
+    //     <ThemeSwitch />
+    //     <MobileNav />
+    //   </div>
+    // </header>
   )
 }
 
